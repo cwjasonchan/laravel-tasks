9a61c6a6d31e08935f82587124c850f569f60655
cwjasonchan <131398398+cwjasonchan@users.noreply.github.com>
2024-04-22 12:18:18 -0600

    Update tasks.blade.php

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -18,10 +18,55 @@
 
                         <!-- Task Name -->
                         <div class="form-group">
-                            <label for="task-name" class="col-sm-3 control-label">Task</label>
+                            <label for="task-name" class="col-sm-3 control-label">Name</label>
 
                             <div class="col-sm-6">
-                                <input type="text" name="name" id="task-name" class="form-control" value="{{ old('task') }}">
+                                <input type="text" name="name" id="task-name" class="form-control" value="{{ old('name') }}">
+                            </div>
+                        </div>
+
+                        <!-- Email -->
+                        <div class="form-group">
+                            <label for="task-email" class="col-sm-3 control-label">Email</label>
+
+                            <div class="col-sm-6">
+                                <input type="email" name="email" id="task-email" class="form-control" value="{{ old('email') }}">
+                            </div>
+                        </div>
+
+                        <!-- Address -->
+                        <div class="form-group">
+                            <label for="task-address" class="col-sm-3 control-label">Address</label>
+
+                            <div class="col-sm-6">
+                                <input type="text" name="address" id="task-address" class="form-control" value="{{ old('address') }}">
+                            </div>
+                        </div>
+
+                        <!-- Postal Code -->
+                        <div class="form-group">
+                            <label for="task-postal-code" class="col-sm-3 control-label">Postal Code</label>
+
+                            <div class="col-sm-6">
+                                <input type="text" name="postal_code" id="task-postal-code" class="form-control" value="{{ old('postal_code') }}">
+                            </div>
+                        </div>
+
+                        <!-- Phone Number -->
+                        <div class="form-group">
+                            <label for="task-phone-number" class="col-sm-3 control-label">Phone Number</label>
+
+                            <div class="col-sm-6">
+                                <input type="text" name="phone_number" id="task-phone-number" class="form-control" value="{{ old('phone_number') }}">
+                            </div>
+                        </div>
+
+                        <!-- Date of Birth -->
+                        <div class="form-group">
+                            <label for="task-date-of-birth" class="col-sm-3 control-label">Date of Birth</label>
+
+                            <div class="col-sm-6">
+                                <input type="date" name="date_of_birth" id="task-date-of-birth" class="form-control" value="{{ old('date_of_birth') }}">
                             </div>
                         </div>
 
@@ -47,13 +92,23 @@
                     <div class="panel-body">
                         <table class="table table-striped task-table">
                             <thead>
-                                <th>Task</th>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Address</th>
+                                <th>Postal Code</th>
+                                <th>Phone Number</th>
+                                <th>Date of Birth</th>
                                 <th>&nbsp;</th>
                             </thead>
                             <tbody>
                                 @foreach ($tasks as $task)
                                     <tr>
                                         <td class="table-text"><div>{{ $task->name }}</div></td>
+                                        <td class="table-text"><div>{{ $task->email }}</div></td>
+                                        <td class="table-text"><div>{{ $task->address }}</div></td>
+                                        <td class="table-text"><div>{{ $task->postal_code }}</div></td>
+                                        <td class="table-text"><div>{{ $task->phone_number }}</div></td>
+                                        <td class="table-text"><div>{{ $task->date_of_birth }}</div></td>
 
                                         <!-- Task Delete Button -->
                                         <td>
